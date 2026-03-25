2f4bac252746a92c7566eee0654534b1f0241fc3
bruno <brunocanalemerick@hotmail.com>
2026-03-25 20:12:00 +0100

    Add data models, DbContext, and database configuration

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Expense> Expenses => Set<Expense>();
+}
